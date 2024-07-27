9ef1585b213142cfe89ef98ac959e843ab122a5a
Kaustubh Patil <105577500+knp-07@users.noreply.github.com>
2024-07-28 00:36:15 +0530

    Update Reverse_a_number.java

--- Basic/Reverse_a_number.java
+++ Basic/Reverse_a_number.java
@@ -1,6 +1,15 @@
 import java.util.*;
 public class Reverse_a_number {
     public static void main(String[] args){
+
+/*To reverse a number you need to perform 3 operations and those are: 
+  1. Get the remainder by dividing the number by 10 i.e use the '%' oprator
+  2. Then create a variable with value zero and multiply it by 10 and add the remainder.
+  3. The value of the variable will be updated after each loop.
+  4. Then divide the number by 10 as the last step and keep repeting the same till number becomes zero.
+  5. Lastly print the variable*/
+        
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number that you wish to reverse: ");
         int number = sc.nextInt();
